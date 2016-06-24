9344d312cfeea18ec58a079f3ca1e0e9d36896a4
cuibin <cuibin@admaster.com.cn>
2016-06-24 17:53:32 +0800

    init

---
+++ server.js
@@ -0,0 +1,39 @@
+var _ = require('lodash')
+var log = require('beautylog')
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+
+var data = [
+]
+
+app.use(bodyParser.json())
+
+app.post('/send-public', (req, res) => {
+	
+	var body = req.body
+
+	found = _.find(data, (x) => body.name == x.name)
+
+	if(!found) {
+		data.push({
+			name: body.name,
+			intranet: body.intranet,
+			public: body.public
+		})
+	} else {
+		if(body.public) found.public = body.public
+		if(body.intranet) found.intranet = body.intranet
+	}
+
+	res.json(data)
+})
+
+app.get('/get-all-adresses', (req, res) => {
+	log.info("data:" + JSON.stringify(data))
+	res.json(data)
+})
+
+app.listen(8087)
+
+log.success('server start at 8087')
